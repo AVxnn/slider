80f76e5752e6b28ca121edf81109e6fa73a850ed
George <52869708+AVxnn@users.noreply.github.com>
2021-02-27 21:18:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let img = document.getElementById('img');
+let left = document.querySelector('.arrow-left');
+let right = document.querySelector('.arrow-right');
+let add = document.querySelector('.add-block');
+let remove = document.querySelector('.remove-block');
+
+
+let length = 3;
+let array = ['img/red.png', 'img/yellow.png', 'img/green.png', 'img/blue.png', 'img/purple.png', 'img/black.png'];
+let i = 0;
+
+
+left.addEventListener('click', function(){
+    if (i <= 0) {
+        i = length;
+    }
+    i--;
+    img.src = array[i];
+})
+
+
+right.addEventListener('click', function(){
+    if (i >= length - 1) {
+        i = -1;
+    }
+    i++;
+    img.src = array[i];
+})
+
+add.addEventListener('click', function(){
+    if(length == array.length) {
+        alert('Максимум')
+
+    } else {
+        length++;
+        img.src = array[length - 1];
+        i = length - 1;
+    }
+})
+
+remove.addEventListener('click', function(){
+    if(length == 1) {
+        alert('Максимум')
+    } else {
+        length--;
+        img.src = array[length - 1];
+        i = length - 1;
+    }
+})
